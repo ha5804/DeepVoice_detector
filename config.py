abe94e7345cd0affe8326ea8e4371e00b37fb2ed
ha5804 <zixpotf5803@gmail.com>
2026-01-21 13:41:42 +0900

    check file

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATASET_ROOT = DATASET_ROOT = "/Users/taehayeong/Desktop/dataset"
+
+
+LJSPEECH_PATH = os.path.join(
+    DATASET_ROOT, "wavs"
+)
